e53dfedc2fce4db157d5d4d14d1cdea13362f4b6
Elizabeth Razo <erazopadilla@equinix.com>
2021-03-07 22:56:08 -0800

    *

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import logo from './logo.svg';
+import {Greeting} from './Greeting'
+import {PeopleList} from './PeopleList'
 import './App.css';
 
+const people =[
+  {
+    name: 'John',
+    age: 40,
+    hairColor: 'brown'
+  },
+  {
+    name: 'Helga',
+    age: 80,
+    hairColor: 'blonde'
+  },
+  {
+    name: 'Ramiro',
+    age: 10,
+    hairColor: 'black'
+  }
+];
+
+
 function App() {
+  const displayAlert = () =>{
+    alert('Hello!');
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <p style={{color: 'red', fontSize: '96px'}}>Big Red Text!</p>
+        <Greeting name="Shaun" numberOfMessages={100} />
+        <PeopleList people={people} />
+        <button onClick= {() => {alert('Hello world!');}}>Click Me!</button>     
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          This is a PoC!
+        </p>        
         <a
           className="App-link"
           href="https://reactjs.org"
